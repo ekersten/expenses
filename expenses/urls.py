d4e3cfae7c42b36bd987b2dedb917086fe190f7b
Eric Kersten <ekersten@gmail.com>
2019-01-02 10:51:01 -0300

    cambios varios

--- expenses/urls.py
+++ expenses/urls.py
@@ -27,5 +27,6 @@ router.register(r'expenses', views.ExpenseViewSet, basename='expense')
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'api/', include(router.urls)),
-    url(r'api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('', include('frontend.urls'))
 ]
---
+++ frontend/src/components/App.js
@@ -0,0 +1,7 @@
+const App = () => (
+    <div>
+        <h1>Expenses</h1>
+    </div>
+)
+
+export default App;
