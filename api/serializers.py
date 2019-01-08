735a9ae929f5f1a5f593b2673de2d75a163bb0cf
Eric Kersten <ekersten@gmail.com>
2019-01-08 12:05:43 -0300

    serializers y popover

--- api/serializers.py
+++ api/serializers.py
@@ -1,20 +1,22 @@
 from core.models import User, Category, Expense
 from rest_framework import serializers
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('url', 'username', 'first_name', 'last_name', 'email', 'groups')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'groups')
 
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name')
 
 
-class ExpenseSerializer(serializers.HyperlinkedModelSerializer):
-    category = serializers.StringRelatedField()
+class ExpenseSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    amount = serializers.FloatField()
+    date = serializers.DateField()
     class Meta:
         model = Expense
         fields = ('id', 'amount', 'category', 'date', 'note', 'created', 'modified')
--- frontend/src/components/ExpenseItem.js
+++ frontend/src/components/ExpenseItem.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import moment from 'moment'
 import numeral from 'numeral'
+import { Popover, PopoverBody, PopoverHeader, Button } from 'reactstrap'
 
 class ExpenseItem extends React.Component {
+
+    state = {
+        popoverOpen: false
+    }
+
+    togglePopover = () => {
+        this.setState(state => ({
+            popoverOpen: !this.state.popoverOpen
+        }))
+    }
 
     render() {
         const { expense } = this.props
         return (
-            <tr>
+            <React.Fragment>
+            <tr onClick={this.togglePopover} id={`Expense${expense.id}`}>
                 <td>{numeral(expense.amount).format('$0,0.00')}</td>
-                <td>{expense.category}</td>
+                <td>{expense.category.name}</td>
                 <td>{moment(expense.date).format('DD/MM/YYYY')}</td>
             </tr>
+                <Popover placement="top" target={`Expense${expense.id}`} isOpen={this.state.popoverOpen}>
+                    <PopoverHeader>{expense.category.name} {numeral(expense.amount).format('$0,0.00')}</PopoverHeader>
+                <PopoverBody>
+                    {expense.note && (
+                        <p>{expense.note}</p>    
+                    )}
+                    <p><Button color="primary">Editar</Button> <Button color="danger">Borrar</Button></p>
+                </PopoverBody>
+            </Popover>
+            </React.Fragment>
         )
     }
     
